9ecb5a8d1f61ab2c1c5af32282381d066b52331c
nobody48sheldor <lolmyizi@gmail.com>
2025-03-16 12:32:08 +0100

    update

--- backend.py
+++ backend.py
@@ -10,5 +10,13 @@ app.config['SECRET_KEY'] = os.urandom(24).hex()
 def home():
     return(render_template("main.html"))
 
+@app.route("/", method=["GET", "POST"])
+def home():
+    reload = request.form.get("pull_and_reload", None)
+    if reload != None:
+        os.system("git pull")
+    return(render_template("main.html"))
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
